cdb7db6237f86ada72f75a0030b935f94169bbb3
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-05-12 13:46:03 -0300

    first commit

---
+++ Api.WebAPi/Data/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Api.WebAPi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.WebAPi.Data
+{
+    
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Pais> Paises { get; set; }
+    }
+}
---
+++ Api.WebAPi/Data/Entities/Pais.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.WebAPi.Data.Entities
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+        public string CodPais { get; set; }
+        public string NomPais { get; set; }
+        
+    }
+}
