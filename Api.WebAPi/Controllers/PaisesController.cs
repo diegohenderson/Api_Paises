1d8f6ad4f702e6685cdb2bff7dda4413c2514603
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-05-12 14:19:49 -0300

    Added some changes on the propierties of the project, and ended with the paisescontroller, And now its working

--- Api.WebAPi/Controllers/PaisesController.cs
+++ Api.WebAPi/Controllers/PaisesController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.WebAPi.Data;
+using Api.WebAPi.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.WebAPi.Controllers
 {
-    public class PaisesController : Controller
+    // api/paises
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ApiContext context;
+        public PaisesController(ApiContext context)
         {
-            return View();
+
+            this.context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Pais>> Get()
+        {
+            return context.Paises.ToList();
         }
     }
 }
--- Api.WebAPi/Data/Entities/Pais.cs
+++ Api.WebAPi/Data/Entities/Pais.cs
@@ -8,6 +8,7 @@ namespace Api.WebAPi.Data.Entities
     public class Pais
     {
         public int Id { get; set; }
+        
         public string CodPais { get; set; }
         public string NomPais { get; set; }
         
