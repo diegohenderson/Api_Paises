07850b47682825448ce03d2b27212076767c1136
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-05-18 11:43:10 -0300

    Added the put and the delete methods.

--- Api.WebAPi/Controllers/PaisesController.cs
+++ Api.WebAPi/Controllers/PaisesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Api.WebAPi.Data;
 using Api.WebAPi.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.WebAPi.Controllers
 {
@@ -26,7 +27,7 @@ namespace Api.WebAPi.Controllers
             return context.Paises.ToList();
         }
         //nuevo con lamda
-        [HttpGet("{id}", Name ="ObtenerPaisPorId")]
+        [HttpGet("{id}", Name = "ObtenerPaisPorId")]
         public ActionResult<Pais> Get(int id)
         {
             var pais = context.Paises.FirstOrDefault(p => p.Id == id);
@@ -40,13 +41,38 @@ namespace Api.WebAPi.Controllers
 
 
         [HttpPost]
-        public ActionResult<Pais>Post([FromBody]Pais pais)
+        public ActionResult<Pais> Post([FromBody]Pais pais)
         {
             context.Paises.Add(pais);
             context.SaveChanges();
-            //return new CreatedAtRouteResult("obtenerPaisPorId", new { id = pais.Id }, pais);
-            return pais;
+            return new CreatedAtRouteResult("obtenerPaisPorId", new { id = pais.Id }, pais);
+            //return pais;
 
+        }
+        //put
+        [HttpPut("{id}")]
+        public ActionResult<Pais> Put(int id, [FromBody] Pais pais)
+        {
+            if (id!= pais.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(pais).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Pais>Delete(int id)
+        {
+            var pais = context.Paises.FirstOrDefault(p => p.Id == id);
+            if (pais==null)     
+            {
+                return NotFound();
+            }
+            context.Paises.Remove(pais);
+            context.SaveChanges();
+            return Ok();
         }
     }
 }
---
+++ Api.WebAPi/Data/Entities/Provincia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.WebAPi.Data.Entities
+{
+    public class Provincia
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Codigo { get; set; }
+        [Required]
+        public string NombreProv { get; set; }
+        public Pais Pais { get; set; }
+    }
+}
